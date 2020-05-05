9bd2f087b4e2bba4bca1193e5126a0f001870c00
jaquelineramosit <51714172+jaquelineramosit@users.noreply.github.com>
2020-05-05 14:54:54 -0300

    Revert "primeiro commit harpia"
    This reverts commit 02bf33d0c01cc097768c3ecd98f6f7fe229107e7.

--- frontend/src/pages/Dashboards/Dashboardv3/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function DashBoardv3() {
-    return (        
-        <div>  
-            dashboard v3
-        </div>     
-    );    
-}
--- frontend/src/views/index.js
+++
@@ -1,9 +0,0 @@
-import { Logon, Register } from '../pages';
-import Widgets from '..pages/Widgets';
-
-export {
-  Register,
-  Logon,
-  Widgets,
-};
-
