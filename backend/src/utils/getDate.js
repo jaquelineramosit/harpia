fcc94e30add3619cbd8b39c0081975b93547e37e
jaquelineramosit <51714172+jaquelineramosit@users.noreply.github.com>
2020-05-05 13:39:42 -0300

    Revert "primeira versao"
    This reverts commit d05e16bee2af4daac23636413dd69c83fb62b509.

--- backend/src/utils/getDate.js
+++
@@ -1,5 +0,0 @@
-const dateFormat = require('dateformat');
-
-module.exports =  function getDate() {
-    return dateFormat(new Date(), "yyyy-mm-dd");
-}
--- backend/src/utils/getPassword.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-module.exports =  function getPassword( password ) {
-
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
-
-    return hash;
-}
