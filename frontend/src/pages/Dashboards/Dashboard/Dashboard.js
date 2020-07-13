b9653002802e6f7cfd979bd3ce89ddefbf70fc16
leonardocelso <52675129+leonardocelso@users.noreply.github.com>
2020-07-13 10:22:11 -0300

    rotas

--- frontend/src/pages/Dashboards/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboards/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Card, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row, Table, Input, FormGroup, Badge  } from 'reactstrap';
+import { Card, CardBody, CardHeader, Col, Row, Input, FormGroup} from 'reactstrap';
 import { BarChart, Area, ComposedChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, LineChart } from 'recharts';
 import api from '../../../services/api';
 import '../../../global.css';
@@ -12,25 +12,16 @@ export default function Dashboard() {
     const usuarioId = localStorage.getItem('userId');
     const data = [
         {
-            name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+            name: 'Page', uv: 4000, pv: 2800, amt: 3400,
         },
         {
-            name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+            name: 'Page', uv: 3000, pv: 1398, amt: 2210,
         },
         {
-            name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+            name: 'Page', uv: 2000, pv: 9800, amt: 2290,
         },
         {
-            name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-        },
-        {
-            name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-        },
-        {
-            name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-        },
-        {
-            name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+            name: 'Page ', uv: 2780, pv: 3908, amt: 2000,
         },
     ];
     const line = [
@@ -191,7 +182,6 @@ export default function Dashboard() {
                                 data={datatable}
                                 striped={true}
                                 highlightOnHover={true}
-                                responsive={true}
                                 pagination={true}
                             />
                         </CardBody>
@@ -202,13 +192,14 @@ export default function Dashboard() {
                         <CardHeader className="links ">
                             <i className="fa fa-line-chart"></i>Oportunidades Ganhas x Perdidas
                         </CardHeader>
-                        <CardBody >
+                        <CardBody>
+                            
                             <BarChart
-                                width={450}
+                                width={300}
                                 height={170}
                                 data={data}
                                 margin={{
-                                    top: 20, left: -20, bottom: 5,
+                                    top: 20, left: -10, bottom: 5,
                                 }}
                             >
                                 <CartesianGrid strokeDasharray="3 3" />
@@ -219,8 +210,11 @@ export default function Dashboard() {
                                 <Legend />
                                 <Bar yAxisId="left" dataKey="pv" fill="#8884d8" />
                                 <Bar yAxisId="right" dataKey="uv" fill="#82ca9d" />
+                                
                             </BarChart>
+                            
                         </CardBody>
+                       
                     </Card>
                     <Card>
                         <CardHeader className="links ">
@@ -228,11 +222,12 @@ export default function Dashboard() {
                         </CardHeader>
                         <CardBody >
                             <LineChart
-                                width={400}
+                                width={280}
                                 height={170}
                                 data={line}
+                                responsive={true}
                                 margin={{
-                                    top: 20, left: -20, bottom: 5,
+                                    top: 20, left: -10, bottom: 5,
                                 }}
                             >
                                 <CartesianGrid strokeDasharray="3 3" />
@@ -251,11 +246,11 @@ export default function Dashboard() {
                         </CardHeader>
                         <CardBody >
                             <ComposedChart
-                                width={400}
+                                width={280}
                                 height={170}
-                                data={data}
+                                data={compo}
                                 margin={{
-                                    top: 20, left: -20, bottom: 5,
+                                    top: 20, left: -10, bottom: 5,
                                 }}
                             >
                                 <CartesianGrid stroke="#f5f5f5" />
